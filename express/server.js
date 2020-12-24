bce5ab0aeed5ed824322f540c8e199b69c4f2b20
Craig Freeman <craigfreeman@gmail.com>
2020-12-24 15:59:53 -0700

    do something

--- express/server.js
+++ express/server.js
@@ -3,11 +3,45 @@ const path = require("path");
 const serverless = require("serverless-http");
 const app = express();
 const bodyParser = require("body-parser");
-const aws = require("./aws");
+// const aws = require("./aws");
 require("dotenv").config();
+require("pg");
+
+var knex = require("knex")({
+  debug: true,
+  client: "pg",
+  connection: {
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  },
+});
+
+const setupDb = () => {
+  return knex("users")
+    .insert({ name: "netlify man", age: 37 })
+    .then((res) => {
+      console.log("inserted!", res);
+    })
+    .then(() => knex("users"))
+    .catch(console.error);
+};
 
 const router = express.Router();
-router.get("/", aws.handler);
+router.get("/", (req, res) => {
+  setupDb()
+    .then((rows) => {
+      console.log("rows!", rows);
+      res.send(rows);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+  // res.writeHead(200, { "Content-Type": "text/html" });
+  // res.write("<h1>Hello from Express.js!</h1>");
+  // res.end();
+});
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/server", router); // path must route to lambda
